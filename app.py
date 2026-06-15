6a62a596a75f54b0aa97df930b1d812de74b94ea
Amr Yasser <amrabofadl1@gmail.com>
2026-06-15 05:46:47 +0300

    Add environment config with dotenv

--- app.py
+++ app.py
@@ -1,13 +1,20 @@
 from flask import Flask, jsonify
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 
 @app.route('/')
 def home():
+    env = os.getenv('APP_ENV')
     return jsonify({
         "message": "Hello from Docker!",
         "developer": "Amr Yasser",
-        "status": "running inside a container"
+        "status": "running inside a container",
+        "environment": env
     })
 
 @app.route('/about')
