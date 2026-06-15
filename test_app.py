5b0cc867df9814a1e80db52b46696ba18b253caf
Amr Yasser <amrabofadl1@gmail.com>
2026-06-15 05:34:25 +0300

    Break test on purpose

--- test_app.py
+++ test_app.py
@@ -1,11 +1,4 @@
-from app import app
-
 def test_home():
     client = app.test_client()
     response = client.get('/')
-    assert response.status_code == 200
-
-def test_about():
-    client = app.test_client()
-    response = client.get('/about')
-    assert response.status_code == 200
+    assert response.status_code == 999  # wrong status code on purpose
